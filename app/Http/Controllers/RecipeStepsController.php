078c120cb0fb96a79cfe0e91859f3d56de94b8a3
chrish <chrish@pop-os.localdomain>
2022-11-06 10:06:55 -0500

    feat: Initial commit, added Breeze, Pages/index/store

---
+++ app/Http/Controllers/RecipeStepsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RecipeSteps;
+use Illuminate\Http\Request;
+
+class RecipeStepsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RecipeSteps  $recipeSteps
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RecipeSteps $recipeSteps)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RecipeSteps  $recipeSteps
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RecipeSteps $recipeSteps)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RecipeSteps  $recipeSteps
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RecipeSteps $recipeSteps)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RecipeSteps  $recipeSteps
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RecipeSteps $recipeSteps)
+    {
+        //
+    }
+}
